bdf7c89e148cfd10e05c426d8bccabc230581769
Zero to singularity <jan@zerotosingularity>
2018-03-05 10:28:35 -0800

    Add setup.py file for distribution

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pdl",
+    version="0.1.0",
+    scripts="[pdl]",
+    author="Zero to singularity",
+    author_email="jan@zerotosingularity.com",
+    url="https://github.com/zerotosingularity/pdl"
+)
