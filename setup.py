c2f6ca31d444b332e705ca6a2756c802ab157a0f
Zero to singularity <jan@zerotosingularity>
2018-03-05 13:42:57 -0800

    fix setup

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
     name="pdl",
-    version="0.1.0",
-    scripts="[pdl]",
+    version="0.1.4",
     author="Zero to singularity",
     author_email="jan@zerotosingularity.com",
-    url="https://github.com/zerotosingularity/pdl"
+    url="https://github.com/zerotosingularity/pdl",
+    description="Python Download Lib",
+    license="MIT",
+    packages=['pdl']
 )
