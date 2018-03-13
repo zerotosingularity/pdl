7539f1fddfb34160fd901b006cfef439ca9d8587
Zero to singularity <jan@zerotosingularity>
2018-03-13 06:22:21 +0100

    bump version to 0.7.0: allow downloads for all files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pdl",
-    version="0.6.0",
+    version="0.7.0",
     author="Zero to singularity",
     author_email="jan@zerotosingularity.com",
     url="https://github.com/zerotosingularity/pdl",
