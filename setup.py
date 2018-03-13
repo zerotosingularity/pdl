84041e7bc88e575ed81f24d4841a0859a085145e
Zero to singularity <jan@zerotosingularity>
2018-03-13 06:24:05 +0100

    bump version 0.7.1: update readme after linting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pdl",
-    version="0.7.0",
+    version="0.7.1",
     author="Zero to singularity",
     author_email="jan@zerotosingularity.com",
     url="https://github.com/zerotosingularity/pdl",
