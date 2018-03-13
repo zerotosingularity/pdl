95fd042e851eb900755ccea2eceea90cc661aabf
Zero to singularity <jan@zerotosingularity>
2018-03-13 04:49:43 +0100

    Pylint fixes + exception raising

--- pdl/pdl.py
+++ pdl/pdl.py
@@ -1,59 +1,73 @@
-import requests
+""" PDL Module download dataset archives """
 
 import zipfile
 import os
 import tarfile
 
-# TODO: constant naming in Python?
-zip_extension = ".zip"
-tar_extension = ".tar"
-tgz_extension = ".tgz"
-tar_gz_extension = ".tar.gz"
+import requests
+
+ZIP_EXTENSION = ".zip"
+TAR_EXTENSION = ".tar"
+TGZ_EXTENSION = ".tgz"
+TAR_GZ_EXTENSION = ".tar.gz"
+
+EMPTY_URL_ERROR = "ERROR: url parameter should not be empty."
+FILE_FORMAT_ERROR = "ERROR: File format currently not supported."
+FILENAME_ERROR = "ERROR: Filename could not be found."
 
 
 def download(url, data_dir="data/", keep_download=False, overwrite_download=False, verbose=False):
+    """ Download and extract the archive from the url """
+
+    if not(url.endswith(ZIP_EXTENSION)
+           or url.endswith(TAR_EXTENSION)
+           or url.endswith(TGZ_EXTENSION)
+           or url.endswith(TAR_GZ_EXTENSION)):
+        raise Exception(FILE_FORMAT_ERROR)
+
+    if url == "":
+        raise Exception(EMPTY_URL_ERROR)
 
     # Get filename from url
     echo("Get the filename from the supplied url", verbose)
 
     filename = get_filename(url)
 
-    file_location = f"{data_dir}{filename}"
-    print(file_location)
+    if filename == "" or filename is None:
+        raise Exception(FILENAME_ERROR)
 
-    # TODO handle execeptions
-    # - no url supplied
-    # - no file name found
-    # - only zip / tar / tar.gz are supported at the moment??
-    # -- Should allow for other downloads as well
+    file_location = get_file_location(data_dir, filename)
+    echo(file_location, verbose)
 
     if os.path.exists(file_location) and not overwrite_download:
         echo(
-            f"The requested file: {filename} already exists in location: {file_location}", verbose)
+            f"The requested file: {filename} already exists in location: {file_location}. \
+            Use: 'overwrite_download=True' to force overwrite", True)
     else:
-        print(file_location)
-        # download file
-        # TODO see if file exists and overwrite file
+        echo(file_location, verbose)
+
         echo(f"Downloading: {url}", verbose)
         resp = requests.get(url, allow_redirects=True, stream=True)
 
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
 
-        zfile = open(file_location, 'wb+')
-        # TODO Puts the whole thing into memory, so need to look out for a better way to stream this to disk
+        zfile = open(file_location, 'wb')
+
         zfile.write(resp.content)
         zfile.close()
 
         echo("finished downloading", verbose)
 
-    if filename.endswith(zip_extension):
-        # unzip file
+    if filename.endswith(ZIP_EXTENSION):
         echo("Extracting zip file.", verbose)
-        zip = zipfile.ZipFile(file_location, 'r')
-        zip.extractall(data_dir)
-        zip.close()
-    elif filename.endswith(tar_extension) or filename.endswith(tar_gz_extension) or filename.endswith(tgz_extension):
+        zipf = zipfile.ZipFile(file_location, 'r')
+        zipf.extractall(data_dir)
+        zipf.close()
+
+    elif (filename.endswith(TAR_EXTENSION)
+          or filename.endswith(TAR_GZ_EXTENSION)
+          or filename.endswith(TGZ_EXTENSION)):
         echo("Extracting tar file.", verbose)
 
         tar = tarfile.open(file_location, 'r')
@@ -70,14 +84,17 @@ def download(url, data_dir="data/", keep_download=False, overwrite_download=Fals
         echo("Keeping the downloaded file.", verbose)
 
 
-def echo(msg, on=True):
-    if on:
+def echo(msg, debug=True):
+    """ Print msg optionally """
+    if debug:
         print(msg)
 
 
 def get_filename(url):
+    """ Get the filename of a url """
     return url[url.rfind("/")+1:]
 
 
 def get_file_location(data_dir, filename):
+    """ Concat data_dir and filename """
     return f"{data_dir}{filename}"
