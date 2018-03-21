9fd97e0ec9ade94954605dcc01efea1cbf88c95c
Zero to singularity <jan@zerotosingularity>
2018-03-21 18:09:33 +0100

    bump version to 0.7.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pdl",
-    version="0.7.3",
+    version="0.7.4",
     author="Zero to singularity",
     author_email="jan@zerotosingularity.com",
     url="https://github.com/zerotosingularity/pdl",
