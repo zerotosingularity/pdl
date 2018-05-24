925b51f6125d9dce3b7eaf85985dae311edf3692
Zero to singularity <jan@zerotosingularity>
2018-05-24 10:15:06 +0200

    add long_description to setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name="pdl",
-    version="0.8.7",
+    version="0.8.9",
     author="Zero to singularity",
     author_email="jan@zerotosingularity.com",
     install_requires=['requests>=2.18.4'],
     url="https://github.com/zerotosingularity/pdl",
     description="Python Download Lib",
+    long_description="Easily download and explore public datasets.",
     license="MIT",
     packages=['pdl']
 )
