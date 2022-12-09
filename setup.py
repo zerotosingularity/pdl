c6cee2d50a62059190b33e176be72b185c1c1a55
zerotosingularity <jan@zerotosingularity.com>
2022-12-09 21:30:09 +0100

    update setup.py: get version from package, update info

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
+exec(open('pdl/version.py').read())
+
+
 setup(
     name="pdl",
-    version="0.8.16",
-    author="Zero to singularity",
-    author_email="jan@zerotosingularity.com",
+    version=__version__,
+    author="Jan Van de Poel",
+    author_email="jan.vandepoel@seeme.ai",
     install_requires=['requests>=2.18.4'],
     url="https://github.com/zerotosingularity/pdl",
-    description="Python Download Lib",
-    long_description="Easily download and explore public datasets.",
+    description="Public Download Library",
+    long_description="Download public datasets & files in one line of code",
     license="MIT",
     packages=['pdl']
 )
